bfd89a6d70ee79c90ed7f688ac30ccea43ea751a
f1kidd <girard1217@gmail.com>
2016-05-19 14:02:49 -0400

    Add files via upload

--- R/fmlogit.R
+++ R/fmlogit.R
@@ -1,35 +1,78 @@
 #' Estimate Fractional Multinomial Logit Models
 #' 
-#' Used to estimate fractional multinomial logit models using quasi-maximum likelihood estimations. Following Wooldridge 
-#' @param y the dependent variable (N*J). Can be a matrix or a named data frame. The first column of the matrix is automatically treated as the baseline.
-#' @param X independent variable (N*K). Can be a matrix or a named data frame. If there is no intercept term in the X, then an intercept term is automatically added. 
-#' @param beta0 Initial value for beta used in optimization. Uses a 1*K(J-1) vector. Default to a vector of zeros. 
-#' @param MLEmethod Method of optimization. Goes into \code{optim(method=MLEmethod))}. Default to "CG", the conjugate gradients method. 
+#' Used to estimate fractional multinomial logit models using quasi-maximum
+#' likelihood estimations following Papke and Wooldridge(1996).
+#' @param y the dependent variable (N*J). Can be a matrix or a named data frame.
+#'   The first column of the matrix is automatically treated as the baseline.
+#' @param X independent variable (N*K). Can be a matrix or a named data frame.
+#'   If there is no intercept term in the X, then an intercept term is
+#'   automatically added.
+#' @param beta0 Initial value for beta used in optimization. Uses a 1*K(J-1)
+#'   vector. Default to a vector of zeros.
+#' @param MLEmethod Method of optimization. Goes into
+#'   \code{optim(method=MLEmethod))}. Default to "CG", the conjugate gradients
+#'   method.
 #' @param maxit Maximum number of iteration.
-#' @param abstol Tolerence. 
+#' @param abstol Tolerence.
 #' @param ... additional parameters that goes into \code{optim()}
-#' @return \code{estimates} A list of J-1 matrices containing parameter estimates, standard errors, and hypothesis testing results. 
-#' @return \code{likelihood} The likelihood value
-#' @return \code{convergence} Convergence diagnostics code
-#' @return \code{count} Dataset information 
-#' @details
-#' For definition of the fmlogit likelihood function, see Maarten Buis's fmlogit page: http://maartenbuis.nl/software/likelihoodFmlogit.pdf
-#' @examples
-#' fmlogit(y,X)
+#' @return The function returns a list of elements below:
+#' @return \code{$estimates} A list of matrices containing parameter estimates,
+#'   standard errors, and hypothesis testing results.
+#' @return \code{$likelihood} The likelihood value
+#' @return \code{$convergence} Convergence diagnostics code
+#' @return \code{$count} Provides dataset information
+#' @details The fractional multinomial model is the expansion of the multinomial
+#' logit to fractional responses. Unlike standard multinomial logit models,
+#' which only considers 0-1 respones, fractional multinomial model considers the
+#' case where the response variable is fractions that sums up to one. Examples
+#' of these type of data are, percentages of budget spent in education, defense,
+#' public health; fractions of a population that have middle school, high
+#' school, college, or post college education, etc.
+#' 
+#' This function follows Papke and Wooldridge(1996)'s paper, in which they
+#' proposed a quasi-maximum likelihood estimator for fractional response data.
+#' The likelihood function used here is a standard multinomial likelihood
+#' function, see \url{http://maartenbuis.nl/software/likelihoodFmlogit.pdf} for
+#' the likelihood used here. Robust standard errors are provided following Papke
+#' and Wooldridge(1996), in which they proposed an asymptotically consistent
+#' estimator of variance.
+#' 
+#' Maximization is done by calling \code{\link{optim}}. It seems that difference
+#' choice of maximization method may yield drastically different parameter
+#' estimates. Using the conjugate gradients method yields the same results as
+#' using Stata's fmlogit package by Maarten Buis.
+#' 
+#' @examples 
+#' require(foreign)
+#' data = read.dta("http://fmwww.bc.edu/repec/bocode/c/citybudget.dta")
+#' X = data[,2:5]
+#' y = data[,6:11]
+#' results1 = fmlogit(y,X)
+#' @references Papke, L. E. and Wooldridge, J. M. (1996), Econometric methods
+#'   for fractional response variables with an application to 401(k) plan
+#'   participation rates. J. Appl. Econ., 11: 619-632.
+#' @export fmlogit
+
+
 
 
 fmlogit <- function(y,X,beta0=NULL,MLEmethod="CG",maxit=50000,abstol=0.0001,...){
-# input X and y as named data.frame will generate nice estimation tables. Input X,Y as matrix is acceptable.
-# automatically treats the first column of y as baseline choice.
-# X should be augmented with a constant term.
-# betas is the initializer for the MLE optimazation process. Default to 0. 
-Xnames = colnames(X); Ynames = colnames(y)
-X = as.matrix(X); y = as.matrix(y)
 
-n = dim(X)[1]; j=dim(y)[2];
+Xnames = colnames(X); ynames = colnames(y)
+X = as.matrix(X); y = as.matrix(y)
+n = dim(X)[1]; j=dim(y)[2]; k=dim(X)[2]
+
+# handles missing value
+xy = cbind(X,y)
+xy = na.omit(xy)
+X = xy[,1:k]; y=xy[,(k+1):(k+j)]
+n = dim(X)[1]
+remove(xy)
+
+# add constant term if necessary
 X = cbind(X,rep(1,n))
 # here k is No. of explanatories, without the constant term. 
-knew = Matrix::rankMatrix(X)[[1]] # rank of X matrix
+knew = qr(X)$rank # rank of X matrix
 k=knew-1
 if(knew<dim(X)[2]){ # X has constant column
   X = X[,1:knew]
@@ -59,7 +102,7 @@ if(length(beta0) != (k+1)*(j-1)) { # user gives a wrong beta0 vector
 # obviously, using different method of optimization can have big impact on parameter estimates. 
 # Using "CG" (conjugate gradients) method yields same estimates as Stata does. 
 opt<-optim(beta0,QMLE,gr=NULL,y,X,method=MLEmethod,
-           control=list(maxit=maxit,abstol=abstol),hessian=TRUE)
+           control=list(maxit=maxit,abstol=abstol))
 
 # reformat beta matrix
 betamat = matrix(opt$par,ncol=k+1,byrow=T) #(j-1)*k
@@ -112,4 +155,3 @@ outlist$count = c(Obs=n,Explanatories=k,Choices=j)
 return(outlist)
 }
 
-#result1 = fmlogit(y,X)
